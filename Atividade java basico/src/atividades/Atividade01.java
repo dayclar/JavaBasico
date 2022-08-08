754f30b4b582fcfa4861a52b0b84dd359351f2ef
dayclar <claradayanamcm07@gmail.com>
2022-08-08 18:33:21 -0300

    Atividade01

---
+++ Atividade java basico/src/atividades/Atividade01.java
@@ -0,0 +1,23 @@
+package atividades;
+
+import java.util.Scanner;
+
+// 1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
+public class Atividade01 {
+	 public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("informe o primeiro número:");
+		int n1 = entrada.nextInt();
+		
+		System.out.println("informe o segundo número:");
+		int n2 = entrada.nextInt();
+		
+		int soma = n1 + n2;
+		
+		System.out.println("A soma dos números é:" + soma);
+		
+		
+	}
+
+}
